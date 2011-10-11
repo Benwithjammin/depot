7b080243cd9a1ed4a7de899350f32419fc4b9118
Ben Porter <jammin1984@gmail.com>
2011-10-11 21:24:29 +0100

    End of iteration C4 exercises

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_globals
+
+  def set_globals
+    @date_time = Time.now.ctime;
+  end
+
 end
--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
   end
 
 end
