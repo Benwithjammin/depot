e73e3d3156229df55c560759a618c4c8017173b1
= <=>
2011-10-15 18:01:18 +0100

    Added cart and relationships to line items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
 
   before_filter :set_controller_globals
@@ -6,5 +7,15 @@ class ApplicationController < ActionController::Base
   def set_controller_globals
     @date_time = Time.now.ctime
   end
+
+  private
+
+    def current_cart
+        Cart.find(session[:cart_id])
+      rescue ActiveRecord::RecordNotFound
+        cart = Cart.create
+        session[:cart_id] = cart.id
+        cart
+    end
 
 end
