3a5e1f43a25dcfe59f0325e897ff737b50574e8b
= <=>
2012-01-11 19:26:14 +0000

    Ajax'd up cart using JQuery

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -51,6 +51,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         format.html { redirect_to(store_url) }
         format.json { render json: @line_item, status: :created, location: @line_item }
+        format.js { @current_item = @line_item }
       else
         format.html { render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
